ab6eab7064bd01b2c0f8f3000d211e81330ccc6d
mikiyas simeneh <mikiyassimeneh147@gmail.com>
2022-04-05 04:28:22 -0900

    add components

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+const Welcome = () => {
+    return (
+      <div>
+        <h1>Welcome</h1>
+      </div>
+    );
+  };
+  export default Welcome;
+  
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Loader } from "./Loader";
+export { default as NavBar } from "./NavBarLoader";
+export { default as Footer } from "./Footer";
+export { default as Welcome } from "./Welcome";
+export { default as Transactionser } from "./Transactions";
+export { default as Loader } from "./Loader";
+
